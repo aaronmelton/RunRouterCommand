a57de100bfd1dce9d9f5d92e40702ef496de2725
Aaron Melton <aaron@aaronmelton.com>
2013-07-15 14:34:20 -0400

    RunRouterCommand.py 1.08 (2013-07-10)
    Changing (correcting) Python environment variable from static to
    dynamic.

--- RunRouterCommand.py
+++ RunRouterCommand.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #
 # RunRouterCommand.py
-# Copyright (C) 2012-2013  Aaron Melton <aaron(at)aaronmelton(dot)com>
+# Copyright (C) 2012-2013 Aaron Melton <aaron(at)aaronmelton(dot)com>
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
